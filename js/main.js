cd28612f4ed57500b955adc99163d21990af6f85
Domenico Capalbo <capalbodomenico@gmail.com>
2020-09-28 20:32:52 +0200

    bonus-complete
    js-jq-ajax-api-musica

--- js/main.js
+++ js/main.js
@@ -1,42 +1,42 @@
 $(document).ready(function() {
+
 // ask to the server an array objects of album music
 var apiObjects = "https://flynn.boolean.careers/exercises/api/array/music";
-  $.ajax({
-    "url": apiObjects,
-    "method": "GET",
-    "success": function(data, status) {
-      // save the array objects inside of a variable
-      var results = data.response;
-      console.log(data.response);
-      Rendered(results);
-    },
-    "error": function(request, status, error) {
-      alert("an error has occured");
-    }
-   });
+   //make an ajax call for the music
+   $.ajax (
+     {
+     "url": apiObjects,
+     "method": "GET",
+     "success": function(data) {
+       // save the array objects inside of a variable
+       var results = data.response;
+       Rendered(results);
+     },
+     "error": function(error) {
+       alert("an error has occured");
+     }
+    });
 
   $("#genre").change(function() {
-
+   //make a variable to save the value of the genre options
    var valueSelect = $(this).val();
-
+   //make some conditions which let appear or disappear the content
     if (valueSelect == "") {
      $(".cd").fadeIn(1500);
     } else {
-      $(".cd").hide();
-      $(".cd[data-genre=‘" + valueSelect + "’]").fadeIn(1500);
+     $(".cd").hide();
+     $(".cd[data-genre=" + valueSelect + "]").fadeIn(1500);
     }
-
  });
-
+   //make a function for handlebars which will append the html
    function Rendered(results) {
      //make a variable to read the html inside the template class
-     var source = $('#entry-template').html();
+     var source = $('#music-template').html();
      //make a variable for handlebars
 	   var template = Handlebars.compile(source);
      //make a cicle to read and insert the objects inside the HTML
      for (var i = 0; i < results.length; i++) {
 		  var html = template(results[i]);
-		  console.log(html);
 		  $('.cds-container').append(html);
    }
   }
